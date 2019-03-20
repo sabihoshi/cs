296dbb58d440525439138f1a794b17275a15e36a
かおり 中野 <kaworinakano@gmail.com>
2019-03-20 15:51:19 +0800

    Create IRC Create Statistics

--- Others/Statistics/Statistic.cs
+++ Others/Statistics/Statistic.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Statistics
 {
-    public partial class Form1 : Form
+    public partial class Statistic : Form
     {
-        public Form1()
+        public Statistic()
         {
             InitializeComponent();
         }
