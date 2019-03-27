b99fd5aca2edaf7e4df78da8b9a6a9a37834c243
かおり 中野 <kaworinakano@gmail.com>
2019-03-27 15:58:37 +0800

    PoC playing mp3 audio
    Try porting it to WPF (and failing)

---
+++ Day 8/CJam/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CJam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(775, 129);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ImageMouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ImageMouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ImageMouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Day 8/CJam/Form1.cs
@@ -0,0 +1,119 @@
+﻿using NAudio.Wave;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using WaveFormRendererLib;
+
+namespace CJam
+{
+    public partial class Form1 : Form
+    {
+        public WaveOut waveOut;
+
+        private Image draggedImage;
+
+        private string fileName = null;
+
+        private Point initialPosition;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileName = openFileDialog1.FileName;
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                return;
+            }
+
+            // 1. Configure Providers
+            MaxPeakProvider maxPeakProvider = new MaxPeakProvider();
+            RmsPeakProvider rmsPeakProvider = new RmsPeakProvider(200); // e.g. 200
+            SamplingPeakProvider samplingPeakProvider = new SamplingPeakProvider(200); // e.g. 200
+            AveragePeakProvider averagePeakProvider = new AveragePeakProvider(4); // e.g. 4
+
+            // 2. Configure the style of the audio wave image
+            StandardWaveFormRendererSettings myRendererSettings = new StandardWaveFormRendererSettings();
+            myRendererSettings.Width = 1080;
+            myRendererSettings.TopHeight = 64;
+            myRendererSettings.BottomHeight = 64;
+
+            myRendererSettings.BackgroundImage = new Bitmap(@"C:\Users\ciit\Pictures\Konachan.com - 218504 arima_kousei a-shacho blonde_hair dark glasses instrument male miyazono_kaori piano shigatsu_wa_kimi_no_uso tree violin.jpg");
+
+            // 3. Define the audio file from which the audio wave will be created and define the providers and settings
+            WaveFormRenderer renderer = new WaveFormRenderer();
+            Image image = renderer.Render(fileName, averagePeakProvider, myRendererSettings);
+
+            // 4. Store the image
+            image.Save(@"../render.png", ImageFormat.Png);
+            pictureBox1.Image = image;
+            // Or jpeg, however PNG is recommended if your audio wave needs transparency
+            // image.Save(@"C:\Users\sdkca\Desktop\myfile.jpg", ImageFormat.Jpeg);
+
+            waveOut = new WaveOut();
+            waveOut.PlaybackStopped += WaveOut_OnPlaybackStopped;
+            var reader = new Mp3FileReader(fileName);
+            waveOut.Init(reader);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (waveOut.PlaybackState == PlaybackState.Paused || waveOut.PlaybackState == PlaybackState.Stopped)
+            {
+                waveOut.Play();
+                button2.Text = "Pause";
+            }
+            else
+            {
+                waveOut.Pause();
+                button2.Text = "Play";
+            }
+        }
+
+        private void ImageMouseDown(object sender, MouseEventArgs e)
+        {
+            if (sender is PictureBox)
+            {
+                var img = sender as PictureBox;
+                var image = img.Image;
+                initialPosition = e.Location;
+                draggedImage = image;
+            }
+        }
+
+        private void ImageMouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggedImage != null)
+            {
+                var mousePosition = e.Location;
+                var offset = mousePosition.X - initialPosition.X;
+                initialPosition = mousePosition;
+                pictureBox1.Location = new Point(pictureBox1.Location.X + offset, pictureBox1.Location.Y);
+            }
+        }
+
+        private void ImageMouseUp(object sender, MouseEventArgs e)
+        {
+            if (draggedImage != null)
+            {
+                Capture = false;
+                draggedImage = null;
+            }
+        }
+
+        private void WaveOut_OnPlaybackStopped(object sender, EventArgs e)
+        {
+            button2.Text = "Play";
+        }
+    }
+}
