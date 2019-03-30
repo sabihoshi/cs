896dd3a4bc10797329abd297c0d643c3bd7d9213
loliprotector <kaworinakano@gmail.com>
2019-03-31 01:49:39 +0800

    Try to learn MVVM Dynamically add images to a listbox and bind correctly Learn how to do bindings

---
+++ Day 8/MVVMTest/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MVVMTest.Models
+{
+    public class PersonModel
+    {
+        public PersonModel(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
