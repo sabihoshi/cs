bd07a88255f31bf6e99cefef068b09553c063548
loliprotector <kaworinakano@gmail.com>
2019-04-17 04:45:38 +0800

    Finish calculation Fix header names Make sure to format to n2

--- Others/Math/Classes/Statistics.cs
+++ Others/Math/Classes/Statistics.cs
@@ -1,30 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using MathOp = System.Math;
 
 namespace Math.Classes
 {
-    public class Statistics
+    public static class Tables
     {
-        public List<Sample> Population = new List<Sample>();
-        public double       Mean      => Population.Select(x => x.X).Average();
-        public double       Variance  => Population.Sum(x => MathOp.Pow(x.X - Mean, 2));
-        public double       Deviation => MathOp.Sqrt(Variance);
+        public static void CreateTable(this DataGrid dg, Statistics statistics)
+        {
+            dg.Columns.Clear();
+            dg.Items.Clear();
+
+            // The columns and bindings for the table
+            var columns = new Dictionary<string, string>
+            {
+                {"X", "X"},
+                {"P(x)", "Probability"},
+                {"μ", "Mean"},
+                {"x-μ", "MeanMinusX"},
+                {"(x-μ)²", "MeanMinusXSquared"},
+                {"(μ-x)²P(x)", "MeanMinusXSquaredProbability"}
+            };
+
+            foreach (var column in columns)
+            {
+                dg.Columns.Add(new DataGridTextColumn()
+                {
+                    Header = column.Key,
+                    Binding = new Binding(column.Value) { StringFormat = "n2" }
+                });
+            }
+
+            foreach (var sample in statistics.Population)
+                dg.Items.Add(new Table.Sampling(sample, statistics.Mean));
+        }
+
+        public class Table
+        {
+            public class Sampling
+            {
+                public Sampling(Sample sample, double mean)
+                {
+                    X = sample.X;
+                    Probability = sample.Probability;
+                    P = sample.P;
+                    Mean = mean;
+                }
+
+                public double Mean { get; set; }
+                public double MeanMinusX => X - Mean;
+                public double MeanMinusXSquared => MeanMinusX * MeanMinusX;
+                public double MeanMinusXSquaredProbability => MeanMinusXSquared * (P.Item1 / (double)P.Item2);
+                public Tuple<int, int> P { get; set; }
+                public string Probability { get; set; }
+                public double X { get; set; }
+            }
+        }
     }
 
     public class Sample
     {
-        public  double          X           { get; set; }
-        private Tuple<int, int> P           { get; set; }
-        public  string          Probability => $"{P.Item1}/{P.Item2}";
-
         public Sample(double x, Tuple<int, int> probability)
         {
             X = x;
             P = probability;
+        }
+
+        public Tuple<int, int> P { get; set; }
+        public string Probability => $"{P.Item1}/{P.Item2}";
+        public double X { get; set; }
+    }
+
+    public class Statistics
+    {
+        public List<Sample> Population = new List<Sample>();
+        public double Deviation => MathOp.Sqrt(Variance);
+        public double Mean => Population.Select(x => x.X).Average();
+        public double Variance => Population.Average(x => MathOp.Pow(x.X - Mean, 2));
+
+        public Statistics(IEnumerable<IEnumerable<int>> input)
+        {
+            var population = input.ToList();
+            var count = population.Count();
+            Population = population
+                        .Select(x => new Sample(x.Average(), new Tuple<int, int>(1, count)))
+                        .ToList();
         }
     }
 }
--- Others/Math/MainWindow.xaml.cs
+++ Others/Math/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
-﻿using System;
+﻿using Math.Classes;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Math.Classes;
 
 namespace Math
 {
@@ -31,50 +20,36 @@ namespace Math
         private static IEnumerable<IEnumerable<T>>
             GetPermutationsWithRept<T>(IEnumerable<T> list, int length)
         {
-            if (length == 1) return list.Select(t => new T[] {t});
+            if (length == 1) return list.Select(t => new T[] { t });
             return GetPermutationsWithRept(list, length - 1)
                .SelectMany(t => list,
-                    (t1, t2) => t1.Concat(new T[] {t2}));
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
 
         private static IEnumerable<IEnumerable<T>>
             GetPermutations<T>(IEnumerable<T> list, int length)
         {
-            if (length == 1) return list.Select(t => new T[] {t});
+            if (length == 1) return list.Select(t => new T[] { t });
             return GetPermutations(list, length - 1)
                .SelectMany(t => list.Where(o => !t.Contains(o)),
-                    (t1, t2) => t1.Concat(new T[] {t2}));
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        private void CalculateDeviation(object sender, RoutedEventArgs e)
         {
             IEnumerable<IEnumerable<int>> result;
-            var inputList = Population.Text.Split(new[] {','}
+            var inputList = Population.Text.Split(new[] { ',' }
               , StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             if (Replacement.IsChecked ?? false)
                 result = GetPermutationsWithRept(inputList, SampleSize.Value ?? 2);
             else
                 result = GetPermutations(inputList, SampleSize.Value ?? 2);
-            if (Combine.IsChecked ?? false)
-            {
-                var count = result.Count();
-                var test = new Statistics()
-                {
-                    Population = result
-                                .GroupBy(inner => inner.Average())
-                                .Select(g => new Sample(g.First().Average(), new Tuple<int, int>(g.Count(), count)))
-                                .ToList()
-                };
-                SDGrid.ItemsSource = test.Population;
-            }
-            else
-            {
-                //var dict = result
-                //          .Select(x => x.Average())
-                //          .ToList(x => x, y => 1);
-                //SDGrid.ItemsSource = dict;
-            }
-        }
 
+            var res = new Statistics(result);
+            SdGrid.CreateTable(res);
+
+            Variance.Text = res.Variance.ToString("n2");
+            Deviation.Text = res.Deviation.ToString("n2");
+        }
     }
 }
